f6ae5af0cd0392355775e8e3249bd052970584fa
ronitrajfr <ronitrajofficial7@gmail.com>
2025-05-25 15:42:26 +0530

    removed comments

--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,10 @@
 import * as net from "net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 const store = new Map<string, string>();
 const expiryStore = new Map<string, number>();
 
-// Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
   //connection.write("+PONG\r\n");
   connection.on("data", (data) => {
     const request = data.toString();
